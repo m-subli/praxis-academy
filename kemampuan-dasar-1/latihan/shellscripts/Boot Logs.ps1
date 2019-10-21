bf9c511e82c01ff1dc4eaf967e061e88c3b16b92
Muhammad Subli <56763949+m-subli@users.noreply.github.com>
2019-10-21 14:00:51 +0700

    Add files via upload

---
+++ kemampuan-dasar-1/latihan/shellscripts/Boot Logs.ps1
@@ -0,0 +1,3 @@
+﻿    Get-WmiObject -Class Win32_OperatingSystem –ComputerName localhost |
+        Select-Object -Property CSName,@{n=”Terakhir Boot”;
+    e={[Management.ManagementDateTimeConverter]::ToDateTime($_.LastBootUpTime)}}
